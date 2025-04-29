ea6e791e513e5046340416eb46e43b4e1b7c865b
fabriceIR250 <irafashafabrice250@gmail.com>
2025-04-29 18:03:05 +0200

    first errror2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,6 @@
 // tailwind.config.js
-module.exports = {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
       extend: {
         animation: {
